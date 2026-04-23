ccd0bfc53abf20b4c546fbd4608fc302bfb07cc9
OscarSaxonBuisness <oscarsaxonbuisnessacc@gmail.com>
2026-04-23 18:46:13 +0100

    Delete Backend directory

--- Backend/Routes/SpecsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getLaptopSpecs } = require("../Controllers/SpecsController");
-const authMiddleware = require("../Middleware/authMiddleware");
-
-router.get("/specs", authMiddleware, getLaptopSpecs);
-
-module.exports = router;
